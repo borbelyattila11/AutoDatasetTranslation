00475f8756ddb8140114761f2bb74e3e2368f2aa
Armand <72499921+brandnewchoppa@users.noreply.github.com>
2024-07-30 20:53:13 +0200

    Add files via upload

---
+++ translator/utils/utils.py
@@ -0,0 +1,33 @@
+import sys
+import time
+import socket
+sys.path.insert(0,r'./')
+from functools import wraps
+
+
+def timeit(func):
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        print(f'Function {func.__name__} Took {total_time:.4f} seconds')
+
+        return result
+    return timeit_wrapper
+
+
+def have_internet(host="8.8.8.8", port=53, timeout=3) -> bool:
+    """
+    Host: 8.8.8.8 (google-public-dns-a.google.com)
+    OpenPort: 53/tcp
+    Service: domain (DNS/TCP)
+    """
+    try:
+        socket.setdefaulttimeout(timeout)
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+        return True
+    except socket.error as ex:
+        print(ex)
+        return False
