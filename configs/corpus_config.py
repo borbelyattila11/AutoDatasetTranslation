987c5025d09d7b4dbd13c429aac642f5950e7804
Armand <72499921+brandnewchoppa@users.noreply.github.com>
2024-07-30 20:57:58 +0200

    Update corpus_config.py

--- configs/corpus_config.py
+++ configs/corpus_config.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.insert(0,r'./')
+
 from .config import Config
 from typing import List, Dict
 from dataclasses import dataclass, asdict, fields
@@ -8,22 +9,23 @@ from dataclasses import dataclass, asdict, fields
 @dataclass
 class CorpusConfig(Config):
 
-    orig_corpus_texts: str = None
+    hu_caps: str = None
     corpus_lengths: int = None
 
     def __post_init__(self) -> None:
         # Post validate
-        self.corpus_lengths = len(self.orig_corpus_texts) if self.orig_corpus_texts is not None else None
+        self.corpus_lengths = len(self.hu_caps) if self.hu_caps is not None else None
 
     @property
     def __repr__(self) -> str:
+        '''
         s = ""
         s += f"\n Text id: {self.qas_id}"
         if self.orig_corpus_texts:
             s += f"\n Text: {self.orig_corpus_texts}"
             s += f"\n Length: {self.corpus_lengths}"
-
-        return s
+        '''
+        return f'Text: {self.hu_caps}'
 
     @property
     def get_dict(self) -> Dict:
