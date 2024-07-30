012b3ad98b38ab3a36e08c70df8da9977a856841
Armand <72499921+brandnewchoppa@users.noreply.github.com>
2024-07-30 19:29:09 +0200

    Update TranslationParser.py

--- scripts/TranslationParser.py
+++ scripts/TranslationParser.py
@@ -1,64 +1,65 @@
-import sys
-sys.path.insert(0,r'./')
+import sys, argparse
+
 from tqdm.auto import tqdm
+from datasets import load_dataset, load_from_disk
 
-from datasets import load_dataset
-
-from configs import CorpusConfig
+from configs import DefaultConfig
 from translator import DataParser
 
-
-PARSER_NAME = "FiqaParser"
-
-
-class FiqaParser(DataParser):
+class DefaultParser(DataParser):
     def __init__(self, file_path: str, output_path: str):
-        super().__init__(file_path, output_path,
-                         parser_name=PARSER_NAME,
-                         target_config=CorpusConfig,    # The data config to be validated to check if self implement "convert" function is correct or not,
-                                                        # you must map the data form to the correct fields of the @dataclass in the configs/corpus_config.py
-                         target_fields=["orig_corpus_texts"],   # The data fields to be translated (The fields belong to CorpusConfig)
+        super().__init__(file_path,
+                         output_path,
+                         parser_name = 'DefaultParser',
+                         target_config = DefaultConfig,
+                         target_fields = [ 'hu_caps' ], # The data fields to be translated
+                         do_translate = True,
+                         no_translated_code = False,
+                         target_lang = 'hu', # Target language
+                         source_lang = 'en') # Source language
 
-                         do_translate=True,
-                         no_translated_code=False,
-                         target_lang="tr",
-                         source_lang="en",)
+    def read(self, path_to_dataset: str) -> None:
+        super(DefaultParser, self).read()
 
-    # Read function must assign data that has been read to self.data_read
-    def read(self) -> None:
-        # The read function must call the read function in DataParser class
-        # I just want to be sure that the file path is correct
-        super(FiqaParser, self).read()
+        self.data_read = load_dataset(path_to_dataset)
 
-        self.data_read = load_dataset("BeIR/fiqa", 'corpus', split='corpus')
-        return None
-
-    # Convert function must assign data that has been converted to self.converted_data
     def convert(self) -> None:
-        # The convert function must call the convert function in DataParser class
-        # I just want to be sure the read function has actually assigned the self.data_read
-        super(FiqaParser, self).convert()
+        super(DefaultParser, self).convert()
 
         data_converted = []
-        for data in tqdm(self.data_read, desc=f"Converting data"):
+        for data in tqdm(self.data_read, desc = f'Converting data'):
             data_dict = {}
 
-            # The DataParser class has an id_generator method which can create random id for you
-            data_dict['qas_id'] = data['_id']
+            # Generate unique id for each entry
+            data_dict['qas_id'] = self.id_generator()
 
-            data_dict['orig_corpus_texts'] = data['text']
+            # We just need to have an integer id, it doesn't matter the value.
+            data_dict['id'] = 0
+            data_dict['urls'] = data['url']
+            data_dict['en_caps'] = data['caption']
+            data_dict['hu_caps'] = data['caption']
+            
             data_dict['corpus_lengths'] = None
             data_converted.append(data_dict)
 
-        # Be sure to assign the final data list to self.converted_data
         self.converted_data = data_converted
 
-        return None
-
-
 if __name__ == '__main__':
-    fiqa_parser = FiqaParser(r"examples/Fiqa/dummy.txt",
-                                          r"examples/Fiqa")
-    fiqa_parser.read()
-    fiqa_parser.convert()
-    fiqa_parser.save
+
+    # Create an argument parser object
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path_to_dataset', help = 'Path to the dataset object.') # Add 'path_to_dataset' argument
+    parser.add_argument('-o', '--output', help = 'Path of the folder to save the translated dataset.') # Folder to save the translated dataset
+    args = parser.parse_args()
+    
+    # Create the dataset parser instance
+    dataset_parser = DefaultParser(r'scripts/dummy.txt', args.path_to_dataset)
+    
+    # Read the dataset
+    dataset_parser.read()
+
+    # Translate the data
+    dataset_parser.convert()
+
+    # Save the dataset
+    dataset_parser.save
