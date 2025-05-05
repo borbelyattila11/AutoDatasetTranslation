28e958ce829ec20c40cbffd0dc8f86179e44fba3
Armand <72499921+brandnewchoppa@users.noreply.github.com>
2025-05-05 18:38:31 +0200

    Create PTConfig.py

---
+++ configs/PTConfig.py
@@ -0,0 +1,38 @@
+import sys
+sys.path.insert(0,r'./')
+
+from .config import Config
+from typing import List, Dict
+from dataclasses import dataclass, asdict, fields
+
+
+@dataclass
+class PTConfig(Config):
+
+    hu_prompt: str = None
+    hu_text: str = None
+    corpus_lengths: int = None
+
+    def __post_init__(self) -> None:
+        # Post validate
+        self.corpus_lengths = len(self.hu_prompt) + len(self.hu_txt) if (self.hu_prompt is not None and self.hu_text) else None
+
+    @property
+    def __repr__(self) -> str:
+        '''
+        s = ""
+        s += f"\n Text id: {self.qas_id}"
+        if self.orig_corpus_texts:
+            s += f"\n Text: {self.orig_corpus_texts}"
+            s += f"\n Length: {self.corpus_lengths}"
+        '''
+        return f'Text: {self.hu_prompt} \n {self.hu_text}'
+
+    @property
+    def get_dict(self) -> Dict:
+        return asdict(self)
+
+    @classmethod
+    def get_keys(cls) -> List[str]:
+        all_fields = fields(cls)
+        return [v.name for v in all_fields]
